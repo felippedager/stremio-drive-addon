6eb30df7a8e8f419a938c1f825d96fbf2cf33a24
felippe_dager <felippe.dager@traivefinance.com>
2025-12-23 14:13:38 -0300

    new script

--- app.py
+++ app.py
@@ -3,40 +3,26 @@ import os
 
 app = Flask(__name__)
 
-# Manifesto do addon
 MANIFEST = {
     "id": "org.stremio.gdrive.personal",
     "version": "1.0.0",
     "name": "Google Drive Personal",
-    "description": "Addon pessoal para reproduzir vídeos do Google Drive",
+    "description": "Addon pessoal para vídeos do Google Drive",
     "logo": "https://stremio.com/website/static/img/stremio-logo-small.png",
-
-    "resources": [
-        "stream"
-    ],
-
-    "types": [
-        "movie",
-        "series"
-    ],
-
+    "resources": ["stream"],
+    "types": ["movie", "series"],
     "catalogs": [],
-
-    "idPrefixes": [
-        "gdrive:"
-    ],
-
-    "behaviorHints": {
-        "configurable": False
-    }
+    "behaviorHints": {"configurable": False}
 }
 
-
-# Rota para fornecer o manifest
 @app.route('/manifest.json')
 def manifest():
     return jsonify(MANIFEST)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route('/stream/<type>/<id>.json')
+def stream(type, id):
+    return jsonify({"streams": []})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
